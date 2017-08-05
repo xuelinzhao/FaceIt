eeba2c1c3e6dc016f5cda31189574b6c0dcc0400
xuelinzhao <zhaoxuelin1994@gmail.com>
2017-08-05 21:35:56 +1000

    Initial Commit

---
+++ FaceItTests/FaceItTests.swift
@@ -0,0 +1,36 @@
+//
+//  FaceItTests.swift
+//  FaceItTests
+//
+//  Created by Xuelin Zhao on 2017/8/5.
+//  Copyright © 2017年 zhaoxuelin. All rights reserved.
+//
+
+import XCTest
+@testable import FaceIt
+
+class FaceItTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
